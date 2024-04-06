411d52cc557fc80511ab13b742957cae3cf5197c
Justin Bax <justin.bax@icloud.com>
2024-04-06 00:24:17 -0400

    added routes

---
+++ src/routes/mentee/dashboard/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	
+	export let data: PageData;
+</script>
+
+<h1 class="text-6xl">hello tony</h1>
+<div>if you see this ur gay</div>
--- src/routes/test/+page.svelte
+++
@@ -1,4 +0,0 @@
-<script lang="ts">
-    import { goto } from "$app/navigation";
-</script>
-<h1 class="text-6xl">Hello</h1>
