51d469131aeb65f0b1be98766b25bb3228b2d863
[annie] <ahn517liu@gmail.com>
2024-04-06 02:00:55 -0400

    i tried doing a progress thing

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,12 @@
+<div class="grid grid-cols-2 gap-4 m-12">
+    <div class="p-8 bg-white rounded-l col-span-2">
+        <h1 class="font-bold font-serif text-5xl text-blue-700">Mentor progress</h1>
+        <div class="p-5 max-w-full bg-blue-100 rounded-lg"></div>
+    </div>
+</div>
+<div class="grid grid-cols-6 gap-4"></div>
+<div
+    class="bg-gradient-to-r from-cyan-500 to-blue-700 p-5 px-1 max-w-2xl ml-20 rounded-lg text-white font-serif text-xl"
+>
+    2 hours completed
+</div>
