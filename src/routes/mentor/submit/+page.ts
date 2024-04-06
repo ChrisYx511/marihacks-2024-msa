65885010bf4165396f1b652774402dae5fd72b01
Justin Bax <justin.bax@icloud.com>
2024-04-06 03:17:44 -0400

    finished mentor backend

--- src/routes/mentor/submit/+page.ts
+++ src/routes/mentor/submit/+page.ts
@@ -1,13 +1,15 @@
-import type { PageLoad } from './$types';
+import type { PageLoad } from './$types'
 import { db } from '$lib/firebase.client'
-import { collection, Query, getDocs, query, where, QuerySnapshot } from "firebase/firestore"
+import { collection, Query, getDocs, query, where, QuerySnapshot } from 'firebase/firestore'
 
 export const load: PageLoad = async ({ url }) => {
-	// TODO types
-	const questionsQuery: Query = query(collection(db, "questions"), where("audience", "!=", "mentee"))
-	const questions: QuerySnapshot = await getDocs(questionsQuery)
+    const questionsQuery: Query = query(
+        collection(db, 'questions'),
+        where('audience', '!=', 'mentee')
+    )
+    const questions: QuerySnapshot = await getDocs(questionsQuery)
 
-	return {
-		"questions": questions.docs
-	}
-};
+    return {
+        questions: questions.docs
+    }
+}
