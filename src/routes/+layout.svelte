04821cc0342eb9acf0c0f181c023f67de0042bf5
Chris Yang <chrisyx511@gmail.com>
2024-04-06 03:16:31 -0400

    Fixed NavBar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
-<script>
+<script lang="ts">
     import { onMount } from 'svelte'
     import '../app.css'
     import { auth } from '$lib/firebase.client'
     import { authStore } from '$lib/stores/userStore'
     import Navbar from './Navbar.svelte'
+    import { redirect } from '@sveltejs/kit'
+    import { goto } from '$app/navigation'
+    import { browser } from '$app/environment'
+    import type { User } from 'firebase/auth'
+    import { page } from '$app/stores'
     onMount(() => {
         const unsubscribe = auth.onAuthStateChanged(async (user) => {
             if (!user) {
@@ -22,6 +27,19 @@
         })
         return unsubscribe
     })
+    let currentUser: User | null
+
+    authStore.subscribe((value) => {
+        currentUser = value.user
+    })
+    /* TODO: Fix This
+    $: if (browser && !currentUser && $page.url.pathname !== '/login') {
+        goto('/login')
+        console.log(
+            'we are running in the browser, not signed in, and not on the sign in page, we are going to the sign in page'
+        )
+    }
+*/
 </script>
 
 <Navbar></Navbar>
